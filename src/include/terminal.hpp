851f199f9c4e7bea2b231e13d97113f7ccb915a9
EthanC2 <ethanrcox@gmail.com>
2023-11-27 13:30:24 -0500

    added basic window dimension query

--- src/include/terminal.hpp
+++ src/include/terminal.hpp
@@ -2,12 +2,17 @@
 
 #include <string>
 
+#include <sys/ioctl.h>
+
 class Terminal
 {
     public:
         Terminal(const std::string &channel_);
         ~Terminal();
+        void update_dimensions();
 
     private:
         std::string channel;
+        unsigned short rows;
+        unsigned short columns;
 };
--- src/terminal.cpp
+++ src/terminal.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 
+#include <signal.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
 #include <ncurses.h>
 
 #include "include/terminal.hpp"
@@ -8,13 +11,30 @@
 Terminal::Terminal(const std::string &channel_)
 {
     channel = channel_;
+    update_dimensions();
 
+    // Initialize NCurses
     initscr();  // initialize ncurses screen
     raw();      // disable line buffering
-    //noecho();   // disable character echoing
+    noecho();   // disable character echoing
 }
 
 Terminal::~Terminal()
 {
+    // Deinitialize NCurses
     endwin();
+}
+
+void Terminal::update_dimensions()
+{
+    struct winsize dimensions;
+
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &dimensions);
+
+    rows = dimensions.ws_row;
+    columns = dimensions.ws_col;
+
+    #ifdef DEBUG
+    std::cout << "Updated window dimensions to " << rows << 'x' << columns << '\n';
+    #endif
 }
